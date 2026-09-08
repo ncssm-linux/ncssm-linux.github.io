8c82567fb0cb12cbeff26893c3d99cf1e7ee62ac
surv <mesoscopic@noreply.codeberg.org>
2026-09-08 11:36:15 -0400

    chore: add repo link

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f3d147f8.js',
+                path_to_root + 'searchindex-f683fca9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
